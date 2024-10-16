a187d2ddabead55ecc61ae8024fbcc66b36a01f4
NatanzinHPS <natan.nh20@gmail.com>
2024-10-16 02:31:22 -0300

    Commit inicial

---
+++ src/main/java/com/upxvoluntariado/sistema_voluntariado/SistemaVoluntariadoApplication.java
@@ -0,0 +1,13 @@
+package com.upxvoluntariado.sistema_voluntariado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVoluntariadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVoluntariadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upxvoluntariado/sistema_voluntariado/SistemaVoluntariadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upxvoluntariado.sistema_voluntariado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVoluntariadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
